7267f8513f0bf8c98ebeca20b100f7c107ab1165
Doğukan Atalay <dogukanatalay46@gmail.com>
2022-07-25 16:36:18 +0300

    single event page added

---
+++ components/events/SingleEvent.vue
@@ -0,0 +1,152 @@
+<template>
+  <section class="blog_wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-8 col-lg-8">
+          <div class="blog_post">
+            <h3>Magazine Design Finish The Cover</h3>
+            <div class="post_by d-flex">
+              <span>By -
+                <a
+                  href="#"
+                  title=""
+                  class="bloger_name"
+                >Cavien Piterson</a></span>
+              <span>Posted On : Fab 14 2019</span>
+              <span><a href="#" title="">Your Category Name</a></span>
+            </div>
+            <img src="@/assets/images/event.jpeg" alt="" class="img-fluid">
+            <div class="postpage_content_wrapper">
+              <div class="social_wrapper">
+                <h4>Share</h4>
+                <ul class="social_items list-unstyled">
+                  <li>
+                    <a href="#"><i class="fab fa-facebook-f fb_icon" /></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fab fa-twitter tw_icon" /></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fab fa-linkedin-in link_icon" /></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fab fa-instagram in_icon" /></a>
+                  </li>
+                </ul>
+              </div>
+              <div class="blog_post_content">
+                <p>
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus id quod maxime placeat facere possimus,
+                  omnis voluptas assumenda est
+                </p>
+                <p>
+                  "But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain was born and I will give
+                  you a complete account of the system, and expound the actual
+                  teachings of the great explorer of the truth, the
+                  master-builder of human happiness.
+                </p>
+                <p class="quite_speech">
+                  <i class="fa fa-quote-right" />Vestibulum ante ipsum primis in
+                  faucibus orci luctus et ultrices posuere cubilia Curae id
+                  efficitur condimentum, dui nisl ullamcorper diam, at molestie
+                  nulla erat gna.
+                </p>
+                <p>
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus id quod maxime placeat facere possimus,
+                  omnis voluptas assumenda est
+                </p>
+                <p>
+                  "But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain was born and I will give
+                  you a complete account of the system, and expound the actual
+                  teachings of the great explorer of the truth, the
+                  master-builder of human happiness.
+                </p>
+                <h3>An Additional Point</h3>
+                <p>
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus id quod maxime placeat facere possimus,
+                  omnis voluptas assumenda est
+                </p>
+                <div class="banner">
+                  <img
+                    src="images/blog/details_2.png"
+                    alt=""
+                    class="img-fluid"
+                  >
+                  <p><span>Scource : </span>Unflash</p>
+                </div>
+                <p>
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus id quod maxime placeat facere possimus,
+                  omnis voluptas assumenda est
+                </p>
+                <p>
+                  "But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain was born and I will give
+                  you a complete account of the system, and expound the actual
+                  teachings of the great explorer of the truth, the
+                  master-builder of human happiness.
+                </p>
+                <div class="post_response_count d-flex justify-content-between">
+                  <ul class="d-flex tags">
+                    <li class="tag_headline">
+                      Tags :
+                    </li>
+                    <li><a href="#" title="">Piterson, </a></li>
+                    <li><a href="#" title="">Cavien, </a></li>
+                    <li><a href="#" title="">human, </a></li>
+                    <li><a href="#" title="">master, </a></li>
+                    <li><a href="#" title="">pleasure</a></li>
+                  </ul>
+                  <ul class="d-flex comnent_count">
+                    <li><i class="fas fa-thumbs-up" />3 Likes</li>
+                    <li><i class="fas fa-comment" />2 Comments</li>
+                  </ul>
+                </div>
+
+                <!-- Blog Comment Wrappper-->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ./ End Blog Area Wrapper-->
+</template>
+
+<script>
+export default {
+  name: 'SingleEvent'
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg" />
+  <EventsSingleEvent />
 </template>
 
 <script>
